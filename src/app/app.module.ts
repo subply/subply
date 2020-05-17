b21ded7b35b64d3e1126c9020a182bf1080fa620
OHHAKO <l36h7129@gmail.com>
2020-05-17 16:24:20 +0900

    chore: code clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UrlInputComponent } from './main/url-input/url-input.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UrlInputComponent } from "./main/url-input/url-input.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UrlInputComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UrlInputComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
