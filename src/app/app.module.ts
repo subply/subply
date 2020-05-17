1cf9c781a2b990f013a3a0cdb994db84cb0bd785
OHHAKO <l36h7129@gmail.com>
2020-05-17 16:52:41 +0900

    feat: create the new component for user translation working.
    register the component to root moudule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UrlInputComponent } from "./main/url-input/url-input.component";
 import { HeaderComponent } from "./header/header.component";
+import { TranslationComponent } from "./main/translation/translation.component";
 
 @NgModule({
-  declarations: [AppComponent, UrlInputComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    UrlInputComponent,
+    HeaderComponent,
+    TranslationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
