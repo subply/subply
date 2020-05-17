ff795bde15c7289aca12eb4c848882da83095c1c
OHHAKO <l36h7129@gmail.com>
2020-05-17 17:00:55 +0900

    feat: create the new component for error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+
 import { AppComponent } from "./app.component";
 import { UrlInputComponent } from "./main/url-input/url-input.component";
 import { HeaderComponent } from "./header/header.component";
 import { TranslationComponent } from "./main/translation/translation.component";
+import { NotFountComponent } from "./main/not-found/not-found.component";
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { TranslationComponent } from "./main/translation/translation.component";
     UrlInputComponent,
     HeaderComponent,
     TranslationComponent,
+    NotFountComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
