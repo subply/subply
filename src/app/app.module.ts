1363aa25fa25e9782f02ae571eb148853ef30755
forbid403 <forbid403@naver.com>
2020-05-18 16:07:14 +0900

    feat: Add router and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { UrlInputComponent } from "./main/url-input/url-input.component";
 import { HeaderComponent } from "./header/header.component";
 import { TranslationComponent } from "./main/translation/translation.component";
 import { NotFoundComponent } from "./main/not-found/not-found.component";
+import { MypageComponent } from './mypage/mypage.component';
+import { RankComponent } from './rank/rank.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { NotFoundComponent } from "./main/not-found/not-found.component";
     HeaderComponent,
     TranslationComponent,
     NotFoundComponent,
+    MypageComponent,
+    RankComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
