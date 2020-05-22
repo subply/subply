d26d073213843aee567a6576b7453db6fe4313df
OHHAKO <l36h7129@gmail.com>
2020-05-22 10:24:30 +0900

    feat: hook method ngOnchanges sample code test
    We can check that parent component catch the child component's changes
    by using binding property.
    create component(parent-HookTestComponent, child-ChildComponent)
    insert the FormsModule to use the input tag into template (component generater
    metadata)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TranslationComponent } from "./main/translation/translation.component";
 import { NotFoundComponent } from "./main/not-found/not-found.component";
 import { MypageComponent } from "./mypage/mypage.component";
 import { RankComponent } from "./rank/rank.component";
+import { HookTestComponent } from "./main/hook-test/hook-test.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "url-input", pathMatch: "full" },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: "translate", component: TranslationComponent },
   { path: "mypage", component: MypageComponent },
   { path: "rank", component: RankComponent },
+  { path: "hook-test", component: HookTestComponent },
 
   { path: "**", component: NotFoundComponent },
 ];
