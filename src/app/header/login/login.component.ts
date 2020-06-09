de8d8df7c0c663864bde29129acb6c765af367e6
forbid403 <forbid403@naver.com>
2020-06-09 16:16:42 +0900

    add: Config file

--- src/app/header/login/login.component.ts
+++ src/app/header/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import config from '../../../config/config.json';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(config.server_url);
   }
   onSubmit(loginForm: NgForm){
     console.log(loginForm);
