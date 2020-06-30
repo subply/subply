22c3b0d022971c0a225b815a92fe950dc7194086
OHHAKO <l36h7129@gmail.com>
2020-06-30 23:31:20 +0900

    chore: Code clean up

--- src/app/main/raw-script/raw-script.component.ts
+++ src/app/main/raw-script/raw-script.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Output, EventEmitter } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { parseString } from "xml2js";
 import { ScriptsService } from "../../../service/scripts.service";
-
+import { ErrorHandlerService } from "../../../service/error-handler.service";
 @Component({
   selector: "app-raw-script",
   templateUrl: "./raw-script.component.html",
@@ -16,7 +15,8 @@ export class RawScriptComponent implements OnInit {
   loading: boolean = true;
 
   constructor(
-    private scriptService: ScriptsService
+    private scriptService: ScriptsService,
+    private errHandlerService: ErrorHandlerService
   ) {}
 
   @Output() scriptEvent = new EventEmitter();
@@ -30,16 +30,27 @@ export class RawScriptComponent implements OnInit {
   loadScripts() {
     if (this.videoId) {
       this.scriptService.getXMLScript(this.videoId).subscribe((xmlScripts) => {
-        if (!xmlScripts) { this.loading = false; return; }
+        if (!xmlScripts) {
+          this.loading = false;
+          return;
+        }
         this.parseScriptsFromXML(xmlScripts);
-        
-        this.scriptService.checkScriptIsExist().subscribe((ret) => {
-          if(ret) return;
-          this.scriptService.initScripts(this.scripts).subscribe((ret)=>{
-            if(!ret) {alert('초기화 실패'); return false;}
-          });
-        });
-  
+
+        this.scriptService.checkScriptIsExist().subscribe(
+          (ret) => {
+            if (ret) return;
+            this.scriptService.initScripts(this.scripts).subscribe((ret) => {
+              if (!ret) {
+                alert("초기화 실패");
+                return false;
+              }
+            });
+          },
+          (error) => {
+            console.log("[checkScriptIsExist 에러]" + error);
+          }
+        );
+
         this.scriptExist = true;
         this.loading = false;
       });
@@ -50,19 +61,19 @@ export class RawScriptComponent implements OnInit {
     parseString(xml_string, { explicitArray: false }, (error, result) => {
       if (error) {
         throw new Error(error);
-      } else {
-        const returned_scripts = result.transcript.text;
-        returned_scripts.map((script) => {
-          const start = parseFloat(script.$.start);
-          const end = start + parseFloat(script.$.dur);
-
-          this.scripts.push({
-            script: script._,
-            startTime: start,
-            endTime: end,
-          });
-        });
       }
+
+      const returned_scripts = result.transcript.text;
+      returned_scripts.map((script) => {
+        const start = parseFloat(script.$.start);
+        const end = start + parseFloat(script.$.dur);
+
+        this.scripts.push({
+          script: script._,
+          startTime: start,
+          endTime: end,
+        });
+      });
     });
   }
 
