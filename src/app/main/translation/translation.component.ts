0f36f5c7e79a482e910b5245264004f557f9bbf9
forbid403 <forbid403@naver.com>
2020-07-05 19:41:35 +0900

    refactor: Load youtube video without library

--- src/app/main/translation/translation.component.ts
+++ src/app/main/translation/translation.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class TranslationComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
+  
   videoId: string;
   player: any;
   scriptIndex: Number;
@@ -18,15 +19,35 @@ export class TranslationComponent implements OnInit {
   }
 
   initPlayer() {
-    const tag = document.createElement("script");
+    const tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
+
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    window.onYouTubeIframeAPIReady = () => this.startVideo();
   }
 
+  startVideo(){
+    this.player = new YT.Player('player', {
+      videoId: this.videoId,
+      playerVars: {
+        rel: 0,
+        autoplay : 0
+      },
+      events: {
+        'onReady': this.onPlayerReady, //로딩할때 이벤트 실행
+        'onStateChange': this.onPlayerStateChange //플레이어 상태 변화시 이벤트실행
+      }
+    });
+  }
   onPlayerReady(event) {
-    event.target.playVideo();
+    // event.target.playVideo();
   }
-
+  onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+    }
+  }
   changeScriptIndex(scriptInfo) {
     this.scriptIndex = scriptInfo.scriptIndex;
   }
