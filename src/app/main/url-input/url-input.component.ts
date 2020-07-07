939844e6c7e785df0e7fe05c42a1149b98a8df4e
forbid403 <forbid403@naver.com>
2020-07-07 23:06:35 +0900

    feat: Move to translate when user pushed enter keyboard

--- src/app/main/url-input/url-input.component.ts
+++ src/app/main/url-input/url-input.component.ts
@@ -10,13 +10,24 @@ export class UrlInputComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.enterClicked();
+  }
+
+  enterClicked(){
+    window.onkeydown = (event)=>{
+      if(event.keyCode === 13){
+        return this.goButtonClicked();
+      }
+    };
+  }
 
   goButtonClicked(){
     
     const url = (<HTMLInputElement>document.getElementById("url")).value;
     let videoId = url.split('=')[1];
-    
+    if(!videoId) return alert('올바른 url을 입력 해 주세요!');
+
     const ampersandPosition = videoId.indexOf('&');
     if(ampersandPosition != -1) {
       videoId = videoId.substring(0, ampersandPosition);
